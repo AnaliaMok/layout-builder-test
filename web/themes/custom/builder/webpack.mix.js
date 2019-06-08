d687fc5d26c4a85b6ee6ecb82f6545a84d4691eb
Analia Mok <analiam319@gmail.com>
2019-06-08 10:16:42 -0400

    Isolated css for layout sections.

--- web/themes/custom/builder/webpack.mix.js
+++ web/themes/custom/builder/webpack.mix.js
@@ -2,7 +2,12 @@ let mix = require("laravel-mix");
 
 const purgecss = require("@fullhuman/postcss-purgecss")({
   // Specify the paths to all of the template files in your project
-  content: ["./templates/**/*.twig", "./layouts/**/*.twig", "./js/**/*.vue"],
+  content: [
+    "./templates/**/*.twig",
+    "./layouts/**/*.twig",
+    "./js/**/*.vue",
+    "../../../modules/custom/my_layouts/layouts/**/*.twig"
+  ],
 
   // Include any special characters you're using in this regular expression
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
